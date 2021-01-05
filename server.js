f244a71bb41a74c6d7f72eccdcb946d8920a705b
lebogang B Olifant <lebogangodeveloper@gmail.com>
2021-01-05 14:54:54 +0200

    serve index file

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ server.use(express.static('public'));
 server.use(express.urlencoded({ extended: true }));
 
 server.get('/', (req, res) => {
-  res.send('<h1>Email API TEST</h1>');
+  res.send(__dirname + 'index.html');
 });
 
 server.get('/api/send', async (req, res) => {
