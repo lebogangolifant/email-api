4bb0885f374606162801906642289725dc9f98e9
lebogang B Olifant <lebogangodeveloper@gmail.com>
2021-01-05 14:02:24 +0200

    ready to deploy

--- server.js
+++ server.js
@@ -1,39 +1,44 @@
-const express = require('express')
-const sendMail = require('./send')
-const config = require('./config.json')
+const express = require('express');
+const sendMail = require('./send');
+const config = require('./config.json');
 
-const server = express()
-const port = 3000
+const server = express();
+//const port = 3000
+// use the express-static middleware
+server.use(express.static('public'));
 
-
-server.use(express.urlencoded({ extended: true })) 
+server.use(express.urlencoded({ extended: true }));
 
 server.get('/', (req, res) => {
-  res.send('Email API TEST')
-})
+  res.send('<h1>Email API TEST</h1>');
+});
 
 server.get('/api/send', async (req, res) => {
-    const r = await sendMail(config)
-    res.send(r)
-  })
+  const r = await sendMail(config);
+  res.send(r);
+});
 
-  server.post('/api/send', async (req, res) => {
-    if (req.body.fromEmail.trim()) {
-      config.data.from.email = req.body.fromEmail
-    }
-    if (req.body.toEmail.trim()) {
-      config.data.personalizations[0].to[0].email = req.body.toEmail
-    }
-    if (req.body.subject.trim()) {
-      config.data.personalizations[0].subject = req.body.subject
-    }
-    if (req.body.content.trim()) {
-      config.data.content[0].value = req.body.content
-    }
-    const r = await sendMail(config)
-    res.send(r)
-  })
+server.post('/api/send', async (req, res) => {
+  if (req.body.fromEmail.trim()) {
+    config.data.from.email = req.body.fromEmail;
+  }
+  if (req.body.toEmail.trim()) {
+    config.data.personalizations[0].to[0].email = req.body.toEmail;
+  }
+  if (req.body.subject.trim()) {
+    config.data.personalizations[0].subject = req.body.subject;
+  }
+  if (req.body.content.trim()) {
+    config.data.content[0].value = req.body.content;
+  }
+  const r = await sendMail(config);
+  res.send(r);
+});
 
-server.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`)
-})
+//server.listen(port, () => {
+//console.log(`Server listening at http://localhost:${port}`);
+//});
+// start the server listening for requests
+server.listen(process.env.PORT || 3000, () =>
+  console.log('Server is running...')
+);
